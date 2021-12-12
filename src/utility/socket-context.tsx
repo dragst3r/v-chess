7edaf4f779a942f8d26f3dec3618783e958bde86
dragst3r <zummi2@gmail.com>
2021-12-12 18:02:17 +0100

    bug fixing for deploy

--- src/utility/socket-context.tsx
+++ src/utility/socket-context.tsx
@@ -1,14 +1,14 @@
+
 import React, { useContext, useEffect, useState } from "react";
 import { io, Socket } from "socket.io-client";
-import { DefaultEventsMap } from "socket.io-client/build/typed-events";
 
 const SocketContext = React.createContext(
-  {} as Socket<DefaultEventsMap, DefaultEventsMap>
+  {} as Socket
 );
 
 const SocketContextProvider: React.FC = ({ children }) => {
   const [socket, setSocket] = useState(
-    {} as Socket<DefaultEventsMap, DefaultEventsMap>
+    {} as Socket
   );
   useEffect(() => {
     const newSocket = io("http://localhost:3000");
@@ -25,6 +25,6 @@ const SocketContextProvider: React.FC = ({ children }) => {
 
 export default SocketContextProvider;
 
-export const useSocket = (): Socket<DefaultEventsMap> => {
+export const useSocket = (): Socket => {
   return useContext(SocketContext);
 };
